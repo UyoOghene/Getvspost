a823842b18631b084954b34072018f7987771908
Uyooghene <uyooghene01@gmail.com>
2024-10-19 09:47:55 +0100

    crud complete

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const app = express();
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 uuidv4(); 
+const methodOverride = require('method-override')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'))
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const comments = [
+let comments = [
     {
         id: uuidv4(),
         username: 'james',
@@ -51,6 +53,40 @@ app.post('/comments', (req, res) => {
     res.redirect('/comments'); 
     // res.send('worked')
 });
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment =  comments.find(c => c.id === id);
+    res.render('comments/edit', {comment});
+
+})
+
+// app.patch('/comments/:id', (req, res) => {
+//     const { id } = req.params;
+//     console.log(req.body.comment)
+//     res.send('goooo')
+//   const comment =  comments.find(c => c.id === id);
+//     res.send('comments/show', {comment});
+// });
+
+app.delete('/comments/:id',(req,res) =>{
+    const {id} = req.params;
+       comments =  comments.filter(c => c.id !== id);
+       res.redirect('/comments')
+
+})
+
+app.patch('/comments/:id',(req,res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id)
+    foundComment.comment = newCommentText;
+    res.redirect('/comments')
+})
+
+// app.patch('/comments/:id',(req,res) => {
+//     res.send('update')
+// })
 
 app.get('/tacos', (req, res) => {
     res.send('Get/tacos response');
